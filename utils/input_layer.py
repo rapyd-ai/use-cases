e609c3077136dc3135f88e2291b98a2537beda56
Sudhendra <sudhendrakambhamettu@gmail.com>
2020-12-21 20:44:33 +0530

    Update input_layer.py

--- utils/input_layer.py
+++ utils/input_layer.py
@@ -2,31 +2,29 @@ import requests
 import json
 from google.colab import files
 
-class InputLayer:
+# Upload from URL
+def file_upload_url(self, URL):
+    r = requests.get(URL, allow_redirects=True)
+    open('input.jpg', 'wb').write(r.content)
+    json_data = {
+        'Option': 'Upload from URL',
+        'Response': r.status_code,
+        'Content': r.url
+    }
 
-    # Upload from URL
-    def file_upload_url(self, URL):
-        r = requests.get(URL, allow_redirects=True)
-        open('input.jpg', 'wb').write(r.content)
-        json_data = {
-            'Option': 'Upload from URL',
-            'Response': r.status_code,
-            'Content': r.url
-        }
-
-        return json_data
+    return json_data
     
-    # Upload from Sys
-    def file_upload_colab(self):
-        json_data = {
-            'Option': 'Upload from sys'
-        }
-        out = json.loads(json_data)
-        uploaded = files.upload()
-        for fn in uploaded.keys():
-            print('User uploaded file "{name}" with length {length} bytes'.format(name=fn, length=len(uploaded[fn])))
-            out.update({'name': fn})
+# Upload from Sys
+def file_upload_colab(self):
+    json_data = {
+        'Option': 'Upload from sys'
+    }
+    out = json.loads(json_data)
+    uploaded = files.upload()
+    for fn in uploaded.keys():
+        print('User uploaded file "{name}" with length {length} bytes'.format(name=fn, length=len(uploaded[fn])))
+        out.update({'name': fn})
         
-        json_data = json.dumps(json_data)
-        return json_data
+    json_data = json.dumps(json_data)
+    return json_data
  
