42124377af52bbea9ad80d6420da209de2483ceb
Sudhendra <sudhendrakambhamettu@gmail.com>
2020-12-21 20:43:24 +0530

    Update ai_service_layer.py

--- utils/ai_service_layer.py
+++ utils/ai_service_layer.py
@@ -1,30 +1,28 @@
 import requests
 
-class AIService:
-
-    def rapydai_vision(self, filepath, task, provider = "gcp", accountid = '', token = ''):
-        url = "https://api.rapyd.ai/v1/vision/"+task
-        payload = {}
-        files = [('file', open(filepath, 'rb'))]
-        headers = {
-            'PROVIDER': provider,
-            'ACCOUNT-ID': accountid,
-            'Authorization': 'Bearer '+ token
-        }
-        response = requests.request("POST", url, headers=headers, data=payload, files=files)
-        return response
+def rapydai_vision(self, filepath, task, provider = "gcp", accountid = '', token = ''):
+    url = "https://api.rapyd.ai/v1/vision/"+task
+    payload = {}
+    files = [('file', open(filepath, 'rb'))]
+    headers = {
+        'PROVIDER': provider,
+        'ACCOUNT-ID': accountid,
+        'Authorization': 'Bearer '+ token
+    }
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    return response
     
-    def rapydai_nlp(self, text, task, provider = "gcp", accountid = '', token = ''):
-        url = "https://api.rapyd.ai/v1/nlp/"+task
-        headers = {
-            'ACCOUNT-ID': 'your-accountid',
-            'Authorization': 'Bearer your-token',
-            'Content-Type': 'application/json'
-        }
-        payload = {
-            "text": text,
-            "provider": provider,
-            "language": "auto"
-        }
-        response = requests.request("POST", url, headers=headers, json = payload)
-        return response
+def rapydai_nlp(self, text, task, provider = "gcp", accountid = '', token = ''):
+    url = "https://api.rapyd.ai/v1/nlp/"+task
+    headers = {
+        'ACCOUNT-ID': 'your-accountid',
+        'Authorization': 'Bearer your-token',
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        "text": text,
+        "provider": provider,
+        "language": "auto"
+    }
+    response = requests.request("POST", url, headers=headers, json = payload)
+    return response
