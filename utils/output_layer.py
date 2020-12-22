c379afc6f2084bcf579af374c2e0f3b3bd7d780e
Sudhendra <sudhendrakambhamettu@gmail.com>
2020-12-22 14:53:34 +0530

    Update output_layer.py

--- utils/output_layer.py
+++ utils/output_layer.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 from IPython.display import display, Markdown, HTML, Image
 import cv2
 
-def display_image(self, filepath):
+def display_image(filepath):
     display(Image(filename=filepath, width = 500))
 
-def display_text(self, text):
+def display_text(text):
     display(HTML("<h2>" + text +"</h2>"))
