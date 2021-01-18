b19e048d1b48e90f82b48d8d2baab06ca5c6603e
Sudhendra <sudhendrakambhamettu@gmail.com>
2021-01-18 12:36:17 +0530

    Update postprocessing_layer.py

--- utils/postprocessing_layer.py
+++ utils/postprocessing_layer.py
@@ -391,6 +391,7 @@ def cv2_transform_image(filepath, bounding_boxes, output_file, transformation):
     color = (0, 0, 255)
     thickness = 2
 
+    bounding_boxes = json.loads(bounding_boxes)["bounding_box"]
     if (bounding_boxes == [] or None):
         pass
     else:
@@ -400,6 +401,6 @@ def cv2_transform_image(filepath, bounding_boxes, output_file, transformation):
             x1 = value['x1']
             x2 = value['x2']
             final = cv2.rectangle(image, (x1, y1), (x2,y2), color, thickness)
-        
+            
         cv2_write_image(final, output_file)
 
